0dcfaf330b94cbed389326a46df9c4e0d2cb226a
Евгений Протозанов <weirdcarrotmonster@gmail.com>
2015-10-19 13:39:05 +0400

    Корректно переписал тесты

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -73,24 +73,6 @@ class TestManagerTaskProcessing(unittest.TestCase):
         s.close()
         return port
 
-    @property
-    def connector(self):
-        class ConnectorContext(object):
-            def __init__(self, loop):
-                self.loop = loop
-
-            def __enter__(self):
-                self.connector = aiohttp.TCPConnector(loop=self.loop)
-                return self.connector
-
-            def __exit__(self, type, value, traceback):
-                self.connector.close()
-
-                if value:
-                    raise value
-
-        return ConnectorContext(self.loop)
-
     @asyncio.coroutine
     def create_server(self, *args, **kwargs):
         port = self.find_unused_port()
@@ -102,178 +84,167 @@ class TestManagerTaskProcessing(unittest.TestCase):
     @coroutine
     def test_handle_request(self):
         url, _ = yield from self.create_server()
-        with self.connector as connector:
-            r = yield from client.options(
-                "{}/".format(url),
-                connector=connector, loop=self.loop)
-            data = yield from r.json()
-            assert data["tasks"] == 0
-            assert data["locks"]["taken"] == 0
-            assert data["locks"]["free"] == 0
+        r = yield from client.options(
+            "{}/".format(url), loop=self.loop)
+        data = yield from r.json()
+        assert data["tasks"] == 0
+        assert data["locks"]["taken"] == 0
+        assert data["locks"]["free"] == 0
 
     @coroutine
     def test_handle_auth(self):
         url, _ = yield from self.create_server(auth=("username", "password"))
-        with self.connector as connector:
-            r = yield from client.options(
-                "{}/".format(url),
-                connector=connector, loop=self.loop, auth=("username", "password"))
-            yield from r.read()
-            assert r.status == 200
+        r = yield from client.options(
+            "{}/".format(url), loop=self.loop, auth=("username", "password"))
+        yield from r.release()
+        assert r.status == 200
 
     @coroutine
     def test_invalid_auth(self):
         url, _ = yield from self.create_server(auth=("username", "password"))
-        with self.connector as connector:
-            r = yield from client.options(
-                "{}/".format(url),
-                connector=connector, loop=self.loop)
-            yield from r.read()
-            assert r.status == 403
-            r = yield from client.options(
-                "{}/".format(url),
-                connector=connector, loop=self.loop, auth=("username", "wrong_password"))
-            yield from r.read()
-            assert r.status == 403
+        r = yield from client.options(
+            "{}/".format(url), loop=self.loop)
+        yield from r.release()
+        assert r.status == 403
+
+        r = yield from client.options(
+            "{}/".format(url), loop=self.loop, auth=("username", "wrong_password"))
+        yield from r.release()
+        assert r.status == 403
 
     @coroutine
     def test_queue_add(self):
         url, _ = yield from self.create_server()
-        with self.connector as connector:
-            t = Task("test_task", [1, 2, 3], "pool", [1], {})
-            r = yield from client.post(
-                "{}/task".format(url), data=json.dumps(t.for_json()),
-                connector=connector, loop=self.loop)
-            data = yield from r.json()
-            assert r.status == 200
-            assert data["result"] == "success"
+        t = Task("test_task", [1, 2, 3], "pool", [1], {})
+        r = yield from client.post(
+            "{}/task".format(url), data=json.dumps(t.for_json()),
+            loop=self.loop)
+        data = yield from r.json()
+        assert r.status == 200
+        assert data["result"] == "success"
 
-            r = yield from client.options(
-                "{}/".format(url), data=json.dumps(t.for_json()),
-                connector=connector, loop=self.loop)
-            data = yield from r.json()
-            assert r.status == 200
-            assert data["tasks"] == 1
+        r = yield from client.options(
+            "{}/".format(url), data=json.dumps(t.for_json()),
+            loop=self.loop)
+        data = yield from r.json()
+        assert r.status == 200
+        assert data["tasks"] == 1
 
     @coroutine
     def test_queue_pool_required(self):
         url, _ = yield from self.create_server()
-        with self.connector as connector:
-            t = Task("test_task", [1, 2, 3], "pool", [1], {})
-            r = yield from client.post(
-                "{}/task".format(url), data=json.dumps(t.for_json()),
-                connector=connector, loop=self.loop)
-            data = yield from r.json()
+        t = Task("test_task", [1, 2, 3], "pool", [1], {})
+        r = yield from client.post(
+            "{}/task".format(url), data=json.dumps(t.for_json()),
+            loop=self.loop)
+        data = yield from r.json()
 
-        with self.connector as connector:
-            r = yield from client.patch(
-                "{}/task/pending".format(url),
-                connector=connector, loop=self.loop)
-            assert r.status == 200
-            data = yield from r.json()
-            assert data is None
+        r = yield from client.patch(
+            "{}/task/pending".format(url),
+            loop=self.loop)
+        assert r.status == 200
+        data = yield from r.json()
+        assert data is None
 
     @coroutine
     def test_queue_view_tasks(self):
         url, _ = yield from self.create_server()
         t1 = Task("test_task", [1, 2, 3], "pool", [1], {})
         t2 = Task("test_task", [1, 2, 3], "pool", [1], {})
-        with self.connector as connector:
-            r = yield from client.post(
-                "{}/task".format(url), data=json.dumps(t1.for_json()),
-                connector=connector, loop=self.loop)
-            data = yield from r.json()
-            assert r.status == 200
-            assert data["result"] == "success"
-        with self.connector as connector:
-            r = yield from client.post(
-                "{}/task".format(url), data=json.dumps(t2.for_json()),
-                connector=connector, loop=self.loop)
-            data = yield from r.json()
-            assert r.status == 200
-            assert data["result"] == "success"
-        with self.connector as connector:
-            r = yield from client.get(
-                "{}/task".format(url),
-                connector=connector, loop=self.loop)
-            assert r.status == 200
-            data = yield from r.json()
-            assert len(data) == 2
-        with self.connector as connector:
-            r = yield from client.get(
-                "{}/task".format(url),
-                params={"limit": 1},
-                connector=connector, loop=self.loop)
-            assert r.status == 200
-            data = yield from r.json()
-            assert len(data) == 1
-            assert data[0]["id"] == str(t1.id)
-        with self.connector as connector:
-            r = yield from client.get(
-                "{}/task".format(url),
-                params={"offset": 1},
-                connector=connector, loop=self.loop)
-            assert r.status == 200
-            data = yield from r.json()
-            assert len(data) == 1
-            assert data[0]["id"] == str(t2.id)
+
+        r = yield from client.post(
+            "{}/task".format(url), data=json.dumps(t1.for_json()),
+            loop=self.loop)
+        data = yield from r.json()
+        assert r.status == 200
+        assert data["result"] == "success"
+
+        r = yield from client.post(
+            "{}/task".format(url), data=json.dumps(t2.for_json()),
+            loop=self.loop)
+        data = yield from r.json()
+        assert r.status == 200
+        assert data["result"] == "success"
+
+        r = yield from client.get(
+            "{}/task".format(url),
+            loop=self.loop)
+        assert r.status == 200
+        data = yield from r.json()
+        assert len(data) == 2
+
+        r = yield from client.get(
+            "{}/task".format(url),
+            params={"limit": 1},
+            loop=self.loop)
+        assert r.status == 200
+        data = yield from r.json()
+        assert len(data) == 1
+        assert data[0]["id"] == str(t1.id)
+
+        r = yield from client.get(
+            "{}/task".format(url),
+            params={"offset": 1},
+            loop=self.loop)
+        assert r.status == 200
+        data = yield from r.json()
+        assert len(data) == 1
+        assert data[0]["id"] == str(t2.id)
 
     @coroutine
     def test_complete_unknown_task(self):
         url, _ = yield from self.create_server()
         t = Task("test_task", [1, 2, 3], "pool", [1], {})
-        with self.connector as connector:
-            r = yield from client.patch(
-                "{}/task/{}".format(url, str(t.id)),
-                data=json.dumps({"stdout": "", "stderr": "", "result": "", "status": "success"}),
-                connector=connector, loop=self.loop)
-            assert r.status == 404
-            data = yield from r.json()
-            assert data["error"] == "Unknown task"
+
+        r = yield from client.patch(
+            "{}/task/{}".format(url, str(t.id)),
+            data=json.dumps({"stdout": "", "stderr": "", "result": "", "status": "success"}),
+            loop=self.loop)
+        assert r.status == 404
+        data = yield from r.json()
+        assert data["error"] == "Unknown task"
 
     @coroutine
     def test_delete_unknown_task(self):
         url, _ = yield from self.create_server()
         t = Task("test_task", [1, 2, 3], "pool", [1], {})
-        with self.connector as connector:
-            r = yield from client.delete(
-                "{}/task/{}".format(url, str(t.id)),
-                connector=connector, loop=self.loop)
-            assert r.status == 404
-            data = yield from r.json()
-            assert data["error"] == "Unknown task"
+
+        r = yield from client.delete(
+            "{}/task/{}".format(url, str(t.id)),
+            loop=self.loop)
+        assert r.status == 404
+        data = yield from r.json()
+        assert data["error"] == "Unknown task"
 
     @coroutine
     def test_delete_task(self):
         url, _ = yield from self.create_server()
         t = Task("test_task", [1, 2, 3], "pool", [1], {})
-        with self.connector as connector:
-            r = yield from client.post(
-                "{}/task".format(url), data=json.dumps(t.for_json()),
-                connector=connector, loop=self.loop)
-            yield from r.json()
 
-        with self.connector as connector:
-            r = yield from client.options(
-                "{}/".format(url),
-                connector=connector, loop=self.loop)
-            data = yield from r.json()
-            assert r.status == 200
-            assert data["tasks"] == 1
+        r = yield from client.post(
+            "{}/task".format(url), data=json.dumps(t.for_json()),
+            loop=self.loop)
+        yield from r.release()
 
-        with self.connector as connector:
-            r = yield from client.delete(
-                "{}/task/{}".format(url, str(t.id)),
-                connector=connector, loop=self.loop)
-            assert r.status == 200
+        r = yield from client.options(
+            "{}/".format(url),
+            loop=self.loop)
+        data = yield from r.json()
+        assert r.status == 200
+        assert data["tasks"] == 1
 
-        with self.connector as connector:
-            r = yield from client.options(
-                "{}/".format(url),
-                connector=connector, loop=self.loop)
-            data = yield from r.json()
-            assert r.status == 200
-            assert data["tasks"] == 0
+        r = yield from client.delete(
+            "{}/task/{}".format(url, str(t.id)),
+            loop=self.loop)
+        yield from r.release()
+        assert r.status == 200
+
+        r = yield from client.options(
+            "{}/".format(url),
+            loop=self.loop)
+        data = yield from r.json()
+        assert r.status == 200
+        assert data["tasks"] == 0
 
     @coroutine
     def test_queue_task_work(self):
@@ -292,56 +263,51 @@ class TestManagerTaskProcessing(unittest.TestCase):
             raise Exception()
 
         t = Task("test_task", [1, 2, 3], "pool", [1], {})
-        with self.connector as connector:
-            r = yield from client.post(
-                "{}/task".format(url), data=json.dumps(t.for_json()),
-                connector=connector, loop=self.loop)
-            assert r.status == 200
-            data = yield from r.json()
-            assert data["result"] == "success"
 
-        with self.connector as connector:
-            r = yield from client.options(
-                "{}/".format(url),
-                connector=connector, loop=self.loop)
-            assert r.status == 200
-            data = yield from r.json()
-            assert data["tasks"] == 1
+        r = yield from client.post(
+            "{}/task".format(url), data=json.dumps(t.for_json()),
+            loop=self.loop)
+        assert r.status == 200
+        data = yield from r.json()
+        assert data["result"] == "success"
 
-        with self.connector as connector:
-            r = yield from client.patch(
-                "{}/task/pending".format(url), params={"pool": "pool"},
-                connector=connector, loop=self.loop)
-            assert r.status == 200
-            data = yield from r.json()
-            assert data["id"] == str(t.id)
+        r = yield from client.options(
+            "{}/".format(url),
+            loop=self.loop)
+        assert r.status == 200
+        data = yield from r.json()
+        assert data["tasks"] == 1
 
-        with self.connector as connector:
-            r = yield from client.options(
-                "{}/".format(url),
-                connector=connector, loop=self.loop)
-            assert r.status == 200
-            data = yield from r.json()
-            assert data["tasks"] == 0
-            assert data["locks"]["taken"] == 3
-            assert data["locks"]["free"] == 0
+        r = yield from client.patch(
+            "{}/task/pending".format(url), params={"pool": "pool"},
+            loop=self.loop)
+        assert r.status == 200
+        data = yield from r.json()
+        assert data["id"] == str(t.id)
 
-        with self.connector as connector:
-            r = yield from client.patch(
-                "{}/task/{}".format(url, str(t.id)),
-                data=json.dumps({"stdout": "", "stderr": "", "result": "", "status": "success"}),
-                connector=connector, loop=self.loop)
-            assert r.status == 200
-            yield from r.json()
+        r = yield from client.options(
+            "{}/".format(url),
+            loop=self.loop)
+        assert r.status == 200
+        data = yield from r.json()
+        assert data["tasks"] == 0
+        assert data["locks"]["taken"] == 3
+        assert data["locks"]["free"] == 0
+
+        r = yield from client.patch(
+            "{}/task/{}".format(url, str(t.id)),
+            data=json.dumps({"stdout": "", "stderr": "", "result": "", "status": "success"}),
+            loop=self.loop)
+        assert r.status == 200
+        yield from r.release()
 
         assert len(result_storage) == 1
 
-        with self.connector as connector:
-            r = yield from client.options(
-                "{}/".format(url),
-                connector=connector, loop=self.loop)
-            assert r.status == 200
-            data = yield from r.json()
-            assert data["tasks"] == 0
-            assert data["locks"]["taken"] == 0
-            assert data["locks"]["free"] == 3
+        r = yield from client.options(
+            "{}/".format(url),
+            loop=self.loop)
+        assert r.status == 200
+        data = yield from r.json()
+        assert data["tasks"] == 0
+        assert data["locks"]["taken"] == 0
+        assert data["locks"]["free"] == 3
